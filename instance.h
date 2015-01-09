0f923abfabb0ba6145c68a25a97c15fd0b50cbf7
kuba0x21 <kuba0x21+git@gmail.com>
2015-01-09 16:55:23 +0100

    Dodana struktura maszyny. Wyodrębniłem funkcję która sprawdza czy zależność następstwa operacji w zadaniu jest spełniona. Warunek idzie zoptymalizować.

--- instance.h
+++ instance.h
@@ -9,6 +9,8 @@
 #define	INSTANCE_H
 
 #include <vector>
+#include <iostream>
+
 
 #define MASZYNA1 1
 
@@ -16,6 +18,7 @@ struct operacja{
     unsigned int czas;     // czas wykonania operacji
     unsigned int maszyna;  // numer maszyny na której ma zostać wykonany
     unsigned int start;    // czas rozpoczęcia operacji
+    unsigned int end;      // czas końca operacji
     unsigned int id;       // identyfikator operacji
     unsigned int zad_id;   // id zadania
     bool visited;          // czy został odwiedzony?
@@ -42,6 +45,26 @@ std::vector<operacja> przerwy;
 maszyna maszyny[2];
 
 unsigned int **feromony;
+
+/**
+ * Sprawdza czy zależność między operacjami jest spełniona
+ * @param op
+ * @return 
+ */
+bool zaleznosc(operacja *op, unsigned int time){
+  /*  std::cout    <<  "Test zależności: P:"
+            <<  op->prev<<" V:"
+            <<  (zadania[op->prev].visited?1:0)
+            <<  " GT:"
+            <<  time
+            <<  " ZT:"
+            <<  zadania[op->prev].end
+            <<  std::endl;*/
+    if(op->prev != -1 && !zadania[op->prev].visited ||
+       op->prev != -1 && zadania[op->prev].visited     && time<zadania[op->prev].end) 
+        return false;
+    return true;
+}
 
 
 #endif	/* INSTANCE_H */
